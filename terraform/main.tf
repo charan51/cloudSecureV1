cd4cd781f31c115591c2c6e3aa2b49362c17f2f1
charan51 <cvcharan51@gmail.com>
2025-03-24 08:43:39 +0530

    kd

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
   region = "us-east-1" # You can change this to your preferred region
 }
-
+data "aws_security_group" "cloudsecure_sg" {
+  name = "cloudsecure-sg-20250323224930"
+}
 resource "aws_instance" "cloudsecure" {
   count         = var.instance_count
-  ami           = "ami-0e86e20dae9224db8" # Ubuntu 22.04 LTS AMI (us-east-1, free tier eligible)
+  ami           = "ami-0230bd60aa48260c6" # Ubuntu 22.04 LTS AMI (us-east-1, free tier eligible)
   instance_type = "t2.micro"             # Free tier eligible
   key_name      = "cloudsecure-key"      # You'll need to create this key pair in AWS
-
   vpc_security_group_ids = [data.aws_security_group.cloudsecure_sg.id]
-
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
